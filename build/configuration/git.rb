2fe45631b256d40215ad3dccffbe3bc12a50dfd1
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-08-06 22:16:51 -0600

    Added the pure ruby tasks

---
+++ build/configuration/git.rb
@@ -0,0 +1,7 @@
+configs ={
+  :git => {
+    :remotes => %w[someone],
+    :repo => "shawaugp"
+  }
+}
+configatron.configure_from_hash(configs)
--- build/tasks/remotes.rb
+++
@@ -1,6 +0,0 @@
-desc "Configure the remote connection to JP"
-
-task :setup_remote_to_jp do
-  `git remote rm jp`
-  `git remote add jp git://github.com/developwithpassion/dwpdp.git`
-end
