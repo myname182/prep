cb7cea7ba02639a82734c2ecef8d74be6e8c8321
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-09-08 14:56:59 -0600

    Updated the default settings for the prep project

--- build/configuration/git.rb
+++ build/configuration/git.rb
@@ -1,7 +1,7 @@
 configs ={
   :git => {
-    :remotes => missing("remotes",__FILE__),
-    :repo => missing("repo",__FILE__)
+    :remotes => potentially_change("remotes",__FILE__),
+    :repo => 'dotnet_bootcamp_prep' 
   }
 }
 configatron.configure_from_hash(configs)
