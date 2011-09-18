c8be878c62fa131880923deec1b8b39a445277ac
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-09-17 20:42:40 -0500

    Adding new configuration scripts

--- build/configuration/git.rb
+++ build/configuration/git.rb
@@ -1,7 +1,8 @@
 configs ={
   :git => {
+    :user => missing("user",__FILE__),
     :remotes => potentially_change("remotes",__FILE__),
-    :repo => 'dotnet_bootcamp_prep' 
+    :repo => 'prep' 
   }
 }
 configatron.configure_from_hash(configs)
