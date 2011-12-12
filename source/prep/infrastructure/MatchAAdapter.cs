7f919547f86fe7cdba785bc20142909048cb5768
codingstorm <email.raza@gmail.com>
2011-12-12 21:45:57 +0000

    Pushing new changes

---
+++ source/prep/infrastructure/MatchAAdapter.cs
@@ -0,0 +1,17 @@
+﻿namespace prep.infrastructure
+{
+    public class MatchAAdapter<item> : IMatchA<item>
+    {
+        private Condition<item> condition;
+
+        public MatchAAdapter(Condition<item> condition)
+        {
+            this.condition = condition;
+        }
+
+        public bool matches(item item)
+        {
+           return condition(item);
+        }
+    }
+}
