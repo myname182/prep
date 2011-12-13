85259f9b98402bbed219cafcd56af7cf4b89bfe5
myname182 <topeter@gmail.com>
2011-12-13 10:27:52 -0800

    Pushing new changes

--- source/prep/infrastructure/EnumerableExtensions.cs
+++ source/prep/infrastructure/EnumerableExtensions.cs
@@ -3,21 +3,22 @@ using prep.infrastructure.matching;
 
 namespace prep.infrastructure
 {
-  public static class EnumerableExtensions
-  {
-    public static IEnumerable<Item> one_at_a_time<Item>(this IEnumerable<Item> items)
+    public static class EnumerableExtensions
     {
-      foreach (var item in items) yield return item;
-    }
+        public static IEnumerable<Item> one_at_a_time<Item>(this IEnumerable<Item> items)
+        {
+            foreach (var item in items) yield return item;
+        }
 
-    public static IEnumerable<Item> all_items_matching<Item>(this IEnumerable<Item> items, IMatchA<Item> criteria)
-    {
-      return items.all_items_matching(criteria.matches);
-    }
+        public static IEnumerable<Item> all_items_matching<Item>(this IEnumerable<Item> items, IMatchA<Item> criteria)
+        {
+            return items.all_items_matching(criteria.matches);
+        }
 
-    static IEnumerable<Item> all_items_matching<Item>(this IEnumerable<Item> items, Condition<Item> condition)
-    {
-      foreach (var item in items) if (condition(item)) yield return item;
+        private static IEnumerable<Item> all_items_matching<Item>(this IEnumerable<Item> items,
+                                                                  Condition<Item> condition)
+        {
+            foreach (var item in items) if (condition(item)) yield return item;
+        }
+
     }
-  }
-}
