fcc23282e894ce98c920478b0a13ea1cc9383bba
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-12-13 09:49:20 -0700

    Updated

--- source/prep/infrastructure/matching/MatchFilteringExtensionPoint.cs
+++ source/prep/infrastructure/matching/MatchFilteringExtensionPoint.cs
@@ -2,7 +2,18 @@
 {
   public class MatchFilteringExtensionPoint<ItemToMatch,PropertyType>
   {
+    public bool negate { get; private set; }
     public Accessor<ItemToMatch, PropertyType> accessor { get; private set; }
+
+    public MatchFilteringExtensionPoint<ItemToMatch,PropertyType> not
+    {
+      get
+      {
+        negate = true;
+        return this;
+      }
+    }
+
 
     public MatchFilteringExtensionPoint(Accessor<ItemToMatch, PropertyType> accessor)
     {
